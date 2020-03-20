1fa0fff1fe9f4daf75310fdb1c388287ab50eac2
Yangxb99 <61459974+Yangxb99@users.noreply.github.com>
2020-03-21 01:23:14 +0800

    Enemy生成
    Assets/Scripts/Enemy
    Tilemap地图绘制
    WayPoint敌人移动路线
    敌人孵化，每波随机
    silder血条

---
+++ Assets/Scripts/Enemy/EnemyCreate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreate : MonoBehaviour
+{
+    public GameObject[] enemys;
+    private float time;//每波间隔时间
+    private float times;//波内每个敌人产生间隔
+    private float count;//波数
+    private float counts;//每波数量
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 2;
+        times = 1;
+        count = 5;
+        counts = 4;
+        StartCoroutine(CreateEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator CreateEnemy()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < counts; j++)
+            {
+                Instantiate(enemys[Random.Range(0, enemys.Length)], transform.position, Quaternion.identity);//随机生成
+                yield return new WaitForSeconds(times);
+            }
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyMotion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMotion : MonoBehaviour
+{
+    public float speed = 5;//设置敌人的速度
+    private Transform[] p;//定义数组
+    private int index = 0;//坐标点
+
+    void Start()
+    {
+        p = WayPoint.p;//调用Waypoint脚本获取节点的位置信息
+    }
+
+    void Update()
+    {
+        Move();//每一帧执行方法
+    }
+
+    void Move()
+    {
+        transform.Translate((p[index].position - transform.position).normalized * Time.deltaTime * speed);//移动，节点到当前位置的向量差的单位差*完成上一帧的时间*速度
+        if (Vector3.Distance(p[index].position, transform.position) < 0.2f)//三维坐标，距离（节点，当前位置）小于0.2f的时候执行
+        {
+            index++;//增加索引，也就获取到下个节点坐标
+            if (index > p.Length - 1)//如果大于最后一个节点时执行
+            {
+                Destroy(this.gameObject);//销毁物体
+            }
+        }
+    }
+}
