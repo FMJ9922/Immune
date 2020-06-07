60be1553006ac07498a2ab84682217c2fa2eb881
Yangxb99 <61459974+Yangxb99@users.noreply.github.com>
2020-06-08 01:45:08 +0800

    资源导入+压缩/JX未完成

---
+++ Assets/Scripts/Charactor/JXCellControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JXCellControl : LRCellBase
+{
+    public GameObject Anti;
+ 
+
+    public override void AttackOneTime()
+    {
+        targetEnemy = ChooseTargetEnemey();
+        if (targetEnemy == null)
+        {
+            cellStatus = CellStatus.Idle;
+            cellAnimator.CleanFrameData();
+            return;
+        }
+        cellAnimator.CleanFrameData();
+        cellStatus = CellStatus.Attack;
+        Invoke("FireWeapon", 1);
+
+    }
+    public override void FireWeapon()
+    {
+        if (targetEnemy == null)
+        {
+            cellStatus = CellStatus.Idle;
+            cellAnimator.CleanFrameData();
+            return;
+        }
+        CheckLeftOrRight(targetEnemy);
+        Vector3 start = transform.position;
+        Vector3 end = targetEnemy.position;
+        //Debug.Log(end);
+        Vector3 control = (start + end) / 2 + new Vector3(0, 0.5f, 0);
+        Anti.SetActive(true);
+        Anti.transform.position = start;
+        float coefficient = JsonIO.GetCoefficiet(cellType, targetEnemy.GetComponent<EnemyMotion>().enemyType);
+        Anti.GetComponent<AntiMotion>().damage = atkDamage * coefficient;
+    }
+}
